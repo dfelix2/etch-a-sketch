e6d5a622a35a2a8873e48ddf43adc3b8ac2f67b1
DF <croissantdixneuf@gmail.com>
2022-10-15 14:54:08 -0700

    Add button that resets grid and lets user create new dimensions

--- javascript.js
+++ javascript.js
@@ -1,6 +1,22 @@
 const container = document.getElementById("container");
+const userChoiceDisplay =document.getElementById('userChoiceDisplay');
+const userGridDisplay = document.createElement('h1');
 
+userChoiceDisplay.append(userGridDisplay);
 
+let scale;
+let userGrid;
+const button = document.querySelector('button');
+button.addEventListener('click', (e) => {
+  deleteGrid();
+  scale = prompt("Set Scale Size");
+    if(scale > 100) {
+      return;
+    }
+  userGrid = scale;
+  userGridDisplay.textContent = 'Grid Size: ' + userGrid + 'x' + userGrid;
+  makeRows(userGrid, userGrid);
+});
 
 let cell;
 function makeRows(rows, cols) {
@@ -8,12 +24,17 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols); 
   for (c = 0; c < (rows * cols); c++) {
    cell = document.createElement("div");
-    cell.textContent = '';
     container.appendChild(cell).id = "grid-item";
   };
 };
 
-makeRows(16, 16);
+function deleteGrid() {
+  let cell = document.querySelectorAll("#grid-item");
+  cell.forEach(cell => {
+    container.removeChild(cell).id = "grid-item";
+  });
+}
+
 
 
 container.addEventListener('mouseover', e => {
